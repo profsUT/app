e2c33972f67a6edaf4bcbc14559b54767a399e30
bryantpeng <bryantpeng@utexas.edu>
2015-03-10 02:28:07 -0500

    Can't believe I messed up the dumb Xcode stuff

--- profsUT/main.m
+++ profsUT/main.m
@@ -1,11 +1,3 @@
-//
-//  main.m
-//  profsUT
-//
-//  Created by Kobe on 2/28/15.
-//  Copyright (c) 2015 Teacher's Pet. All rights reserved.
-//
-
 #import <UIKit/UIKit.h>
 #import "AppDelegate.h"
 
